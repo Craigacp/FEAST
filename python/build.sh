7074a51b99afabd7d371baae78d6994a50ee99d6
Brent Barbachem <bbarbach@redhat.com>
2022-04-17 21:00:16 -0400

    Added data to the build script.

--- python/build.sh
+++ python/build.sh
@@ -1,13 +1,17 @@
 # Script to build all of the dependencies from scratch in the event that
 # nothing has been previously established.
 
+me=$(whoami)
+
+echo "[INFO] Running script as ${me}"
+
 original_dir=$(pwd)
 
 DEP_DIR="../MIToolbox"
 
 if [ -d "$DEP_DIR" ]; then
     if [ "$(ls -A $DEP_DIR)" ]; then
-	echo "Submodule already initialized";
+	echo "[DEBUG] Submodule already initialized";
     else
 	cd ..;
 	git submodule init;
@@ -18,15 +22,56 @@ fi
 
 # build the dependencies first
 cd $DEP_DIR;
-make clean && make && sudo make install;
+make clean && make;
+
+if [ "$me" = "root" ] ; then
+    echo "[INFO] Running make install as root";
+    make install;
+else
+    sudo make install;
+fi
 
 # Move to the home directory
 cd ..;
-make clean && make && sudo make install;
+make clean && make;
 
-# These were more than likely added to /usr/local/lib
-export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib;
+if [ "$me" = "root" ] ; then
+    make install;
+else
+    echo "[INFO] Running make install as root";
+    sudo make install;
+fi
+
+if [[ -z "${LD_LIBRARY_PATH}" ]] ; then
+    # These were more than likely added to /usr/local/lib
+    export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib;    
+else
+    if [[ ":$LD_LIBRARY_PATH:" == *":/lib:"* ]] ; then
+	echo "[DEBUG] /lib is already part of LD_LIBRARY_PATH"
+    else
+	export LD_LIBRARY_PATH=/lib:$LD_LIBRARY_PATH
+    fi
+
+    if [[ ":$LD_LIBRARY_PATH:" == *":/usr/lib:"* ]] ; then
+	echo "[DEBUG] /usr/lib is already part of LD_LIBRARY_PATH"
+    else
+	export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH
+    fi
+
+    if [[ ":$LD_LIBRARY_PATH:" == *":/usr/local/lib:"* ]] ; then
+	echo "[DEBUG] /usr/local/lib is already part of LD_LIBRARY_PATH"
+    else
+	export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+    fi
+fi
+
 
 # build the python data !
 cd $original_dir;
-python3 -m pip install . --upgrade;
+
+if [ "$me" = "root" ] ; then
+    python3 -m pip install --upgrade;
+else
+    echo "[INFO] Running python install as root";
+    sudo python3 -m pip install . --upgrade;
+fi
