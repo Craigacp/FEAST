69a00a34e9e84c1b59133b08eb4dcd32492b4484
Brent Barbachem <barbacbd@dukes.jmu.edu>
2022-08-29 09:16:21 -0400

    Update the python setup.
    ** New versions of Python (3.10) were having issues witht the LICENSE reference.
    ** Changed to use the setup.cfg file instead which appears to have more flexibility.
    ** Removed the make commands from setup.py. It wasn't referencing the correct files.
    ** Moved name of the package to fstoolbox so that name collisions do not occur.
    ** Added a requirements.txt. Technically it serves a different purpose than using the setup to install.
    ** README: fix the example
    
    ** Remove source of the bashrc

--- python/setup.py
+++ python/setup.py
@@ -1,34 +1,6 @@
-import subprocess
-import sys
 from setuptools import setup
-from setuptools.command.install import install
-from os.path import join
-from os import getcwd
 
 
-# Read license data to match the dependent project
-with open(join(getcwd(), "../LICENSE"), "r") as license_file:
-    license_data = license_file.read()
-
-
-class FEASTInstall(install):
-    def run(self):
-        if sys.platform.startswith('win'):
-            subprocess.run(['make', 'x64_win', 'install'])
-        else:
-            subprocess.run(['make', 'install'])
-        install.run(self)
-
-
-setup(
-    name='feast',
-    version='2.1.0',
-    packages=['feast'],
-    url='https://github.com/Craigacp/FEAST',
-    license=license_data,
-    author='Adam Pocock',
-    author_email='adam@adampocock.com',
-    description='An information theoretic feature selection toolbox.',
-    install_requires=['numpy', 'pandas', 'scikit-learn'],
-    cmd_class={'install': FEASTInstall},
-)
+if __name__ == '__main__':
+    # Let setuptools/setup handle the setup.cfg data
+    setup()
